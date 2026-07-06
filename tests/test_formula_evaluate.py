08ce287f2c9666a45d77eb7aa58cd58c347c9681
kraysent <kraysent@gmail.com>
2026-07-06 22:20:44 +0100

    a bit better tests

--- tests/test_formula_evaluate.py
+++ tests/test_formula_evaluate.py
@@ -28,55 +28,64 @@ def evaluate_expr(source: str, columns: dict[str, Col]) -> object:
     return evaluate(parse(source), built)
 
 
-def _sample_columns() -> dict[str, Col]:
-    return {
-        "logd25": Col(1.5),
-        "logr25": Col(0.3),
-        "e_logd25": Col(0.05),
-        "e_logr25": Col(0.04),
-        "pa": Col(190.0, "deg"),
-    }
-
-
-def _log_columns(log_unit: str) -> dict[str, Col]:
-    return {
-        **_sample_columns(),
-        "logd25": Col(1.5, log_unit),
-        "logr25": Col(0.3, log_unit),
-        "e_logd25": Col(0.05, log_unit),
-        "e_logr25": Col(0.04, log_unit),
-    }
-
-
-def _hyperleda_columns() -> dict[str, Col]:
-    return {
-        "logd25": Col(0.697, "dex(0.1 arcmin)"),
-        "logr25": Col(0.13, "dex"),
-        "e_logd25": Col(0.079, "dex(0.1 arcmin)"),
-        "e_logr25": Col(0.028, "dex"),
-        "pa": Col(161.14, "deg"),
-    }
+_COLUMNS: dict[str, Col] = {
+    "logd25": Col(1.5),
+    "logr25": Col(0.3),
+    "e_logd25": Col(0.05),
+    "e_logr25": Col(0.04),
+    "pa": Col(190.0, "deg"),
+    "logd25_mag": Col(1.5, "mag"),
+    "logr25_mag": Col(0.3, "mag"),
+    "e_logd25_mag": Col(0.05, "mag"),
+    "e_logr25_mag": Col(0.04, "mag"),
+    "logd25_dex": Col(1.5, "dex"),
+    "logr25_dex": Col(0.3, "dex"),
+    "e_logd25_dex": Col(0.05, "dex"),
+    "e_logr25_dex": Col(0.04, "dex"),
+    "logd25_hl": Col(0.697, "dex(0.1 arcmin)"),
+    "logr25_hl": Col(0.13, "dex"),
+    "e_logd25_hl": Col(0.079, "dex(0.1 arcmin)"),
+    "e_logr25_hl": Col(0.028, "dex"),
+    "pa_hl": Col(161.14, "deg"),
+    "bri25": Col(23.162, "mag / arcsec2"),
+    "designation": Col("NGC 123"),
+    "str_a": Col("M"),
+    "str_b": Col("82"),
+    "id": Col("82"),
+    "ngc_a": Col("NGC"),
+    "ngc_b": Col("905"),
+    "x_str": Col("hello"),
+    "x_dim": Col(1.5),
+    "x_deg": Col(1.5, "deg"),
+    "x_mag": Col(1.5, "mag"),
+    "x_dex": Col(1.5, "dex"),
+    "x_dex_fn": Col(0.697, "dex(0.1 arcmin)"),
+    "pi": Col(1.0),
+    "pa_trig": Col(30.0, "deg"),
+    "err_name": Col("x"),
+    "x_dimless": Col(0.5),
+}
 
 
 EVAL_CASES: list[EvalCase] = [
     EvalCase(
         name="isophotal_major_axis",
         expression='3 * 10 ** col("logd25") * arcsec',
-        columns=_sample_columns(),
+        columns=_COLUMNS,
         result_val=94.8683,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="isophotal_major_axis_error",
         expression='3 * 10 ** col("logd25") * 2.302585093 * e_logd25 * arcsec',
-        columns=_sample_columns(),
+        columns=_COLUMNS,
         result_val=10.9221,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="isophotal_minor_axis",
         expression='3 * 10 ** (col("logd25") - col("logr25")) * arcsec',
-        columns=_sample_columns(),
+        columns=_COLUMNS,
         result_val=47.5468,
         result_unit=u.arcsec,
     ),
@@ -86,187 +95,187 @@ EVAL_CASES: list[EvalCase] = [
             '3 * 10 ** (col("logd25") - col("logr25")) * 2.302585093 '
             '* (col("e_logd25") ** 2 + col("e_logr25") ** 2) ** 0.5 * arcsec'
         ),
-        columns=_sample_columns(),
+        columns=_COLUMNS,
         result_val=7.0102,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="position_angle_modulo",
         expression='col("pa") % (180.0 * deg)',
-        columns=_sample_columns(),
+        columns=_COLUMNS,
         result_val=10.0,
         result_unit=u.deg,
     ),
     EvalCase(
         name="isophotal_major_axis_mag_units",
-        expression='3 * 10 ** col("logd25") * arcsec',
-        columns=_log_columns("mag"),
+        expression='3 * 10 ** col("logd25_mag") * arcsec',
+        columns=_COLUMNS,
         result_val=94.8683,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="isophotal_major_axis_error_mag_units",
-        expression='3 * 10 ** col("logd25") * 2.302585093 * e_logd25 * arcsec',
-        columns=_log_columns("mag"),
+        expression='3 * 10 ** col("logd25_mag") * 2.302585093 * e_logd25_mag * arcsec',
+        columns=_COLUMNS,
         result_val=10.9221,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="isophotal_minor_axis_error_mag_units",
         expression=(
-            '3 * 10 ** (col("logd25") - col("logr25")) * 2.302585093 '
-            '* (col("e_logd25") ** 2 + col("e_logr25") ** 2) ** 0.5 * arcsec'
+            '3 * 10 ** (col("logd25_mag") - col("logr25_mag")) * 2.302585093 '
+            '* (col("e_logd25_mag") ** 2 + col("e_logr25_mag") ** 2) ** 0.5 * arcsec'
         ),
-        columns=_log_columns("mag"),
+        columns=_COLUMNS,
         result_val=7.0102,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="isophotal_major_axis_dex_units",
-        expression='3 * 10 ** col("logd25") * arcsec',
-        columns=_log_columns("dex"),
+        expression='3 * 10 ** col("logd25_dex") * arcsec',
+        columns=_COLUMNS,
         result_val=94.8683,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="isophotal_major_axis_error_dex_units",
-        expression='3 * 10 ** col("logd25") * 2.302585093 * e_logd25 * arcsec',
-        columns=_log_columns("dex"),
+        expression='3 * 10 ** col("logd25_dex") * 2.302585093 * e_logd25_dex * arcsec',
+        columns=_COLUMNS,
         result_val=10.9221,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="isophotal_minor_axis_error_dex_units",
         expression=(
-            '3 * 10 ** (col("logd25") - col("logr25")) * 2.302585093 '
-            '* (col("e_logd25") ** 2 + col("e_logr25") ** 2) ** 0.5 * arcsec'
+            '3 * 10 ** (col("logd25_dex") - col("logr25_dex")) * 2.302585093 '
+            '* (col("e_logd25_dex") ** 2 + col("e_logr25_dex") ** 2) ** 0.5 * arcsec'
         ),
-        columns=_log_columns("dex"),
+        columns=_COLUMNS,
         result_val=7.0102,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="hyperleda_major_axis",
-        expression='3 * 10 ** col("logd25") * arcsec',
-        columns=_hyperleda_columns(),
+        expression='3 * 10 ** col("logd25_hl") * arcsec',
+        columns=_COLUMNS,
         result_val=14.9321,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="hyperleda_major_axis_error",
-        expression='3 * 10 ** col("logd25") * 2.302585093 * e_logd25 * arcsec',
-        columns=_hyperleda_columns(),
+        expression='3 * 10 ** col("logd25_hl") * 2.302585093 * e_logd25_hl * arcsec',
+        columns=_COLUMNS,
         result_val=2.7162,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="hyperleda_minor_axis",
-        expression='3 * 10 ** (col("logd25") - col("logr25")) * arcsec',
-        columns=_hyperleda_columns(),
+        expression='3 * 10 ** (col("logd25_hl") - col("logr25_hl")) * arcsec',
+        columns=_COLUMNS,
         result_val=11.0693,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="hyperleda_minor_axis_error",
         expression=(
-            '3 * 10 ** (col("logd25") - col("logr25")) * 2.302585093 '
-            '* (col("e_logd25") ** 2 + col("e_logr25") ** 2) ** 0.5 * arcsec'
+            '3 * 10 ** (col("logd25_hl") - col("logr25_hl")) * 2.302585093 '
+            '* (col("e_logd25_hl") ** 2 + col("e_logr25_hl") ** 2) ** 0.5 * arcsec'
         ),
-        columns=_hyperleda_columns(),
+        columns=_COLUMNS,
         result_val=2.1363,
         result_unit=u.arcsec,
     ),
     EvalCase(
         name="surface_brightness_column",
         expression='col("bri25")',
-        columns={"bri25": Col(23.162, "mag / arcsec2")},
+        columns=_COLUMNS,
         result_val=23.162,
         result_unit=u.Unit("mag/arcsec2"),
     ),
     EvalCase(name="string_literal", expression='"abc"', columns={}, result_val="abc"),
     EvalCase(
         name="string_column",
-        expression='col("name")',
-        columns={"name": Col("NGC 123")},
+        expression='col("designation")',
+        columns=_COLUMNS,
         result_val="NGC 123",
     ),
     EvalCase(
         name="string_concat_columns",
-        expression='col("a") + col("b")',
-        columns={"a": Col("M"), "b": Col("82")},
+        expression='col("str_a") + col("str_b")',
+        columns=_COLUMNS,
         result_val="M82",
     ),
     EvalCase(
         name="string_concat_literal_prefix",
         expression='"M " + col("id")',
-        columns={"id": Col("82")},
+        columns=_COLUMNS,
         result_val="M 82",
     ),
     EvalCase(
         name="string_concat_literal_separator",
-        expression='col("a") + " " + col("b")',
-        columns={"a": Col("NGC"), "b": Col("905")},
+        expression='col("ngc_a") + " " + col("ngc_b")',
+        columns=_COLUMNS,
         result_val="NGC 905",
     ),
     EvalCase(
         name="column_string_value",
-        expression='col("x")',
-        columns={"x": Col("hello")},
+        expression='col("x_str")',
+        columns=_COLUMNS,
         result_val="hello",
     ),
     EvalCase(
         name="column_dimensionless",
-        expression='col("x")',
-        columns={"x": Col(1.5)},
+        expression='col("x_dim")',
+        columns=_COLUMNS,
         result_val=1.5,
         result_unit=u.dimensionless_unscaled,
     ),
     EvalCase(
         name="column_angle_unit",
-        expression='col("x")',
-        columns={"x": Col(1.5, "deg")},
+        expression='col("x_deg")',
+        columns=_COLUMNS,
         result_val=1.5,
         result_unit=u.deg,
     ),
     EvalCase(
         name="column_mag_unit",
-        expression='col("x")',
-        columns={"x": Col(1.5, "mag")},
+        expression='col("x_mag")',
+        columns=_COLUMNS,
         result_val=1.5,
         result_unit=u.dimensionless_unscaled,
     ),
     EvalCase(
         name="column_dex_unit",
-        expression='col("x")',
-        columns={"x": Col(1.5, "dex")},
+        expression='col("x_dex")',
+        columns=_COLUMNS,
         result_val=1.5,
         result_unit=u.dimensionless_unscaled,
     ),
     EvalCase(
         name="column_dex_function_unit",
-        expression='col("x")',
-        columns={"x": Col(0.697, "dex(0.1 arcmin)")},
+        expression='col("x_dex_fn")',
+        columns=_COLUMNS,
         result_val=0.697,
         result_unit=u.dimensionless_unscaled,
     ),
     EvalCase(
         name="constant_over_column",
         expression="pi",
-        columns={"pi": Col(1.0)},
+        columns=_COLUMNS,
         result_val=3.1416,
         result_unit=u.dimensionless_unscaled,
     ),
     EvalCase(
         name="column_over_constant",
         expression='col("pi")',
-        columns={"pi": Col(1.0)},
+        columns=_COLUMNS,
         result_val=1.0,
         result_unit=u.dimensionless_unscaled,
     ),
     EvalCase(
         name="trig_on_angle_column",
-        expression='sin(col("pa"))',
-        columns={"pa": Col(30.0, "deg")},
+        expression='sin(col("pa_trig"))',
+        columns=_COLUMNS,
         result_val=0.5,
         result_unit=u.dimensionless_unscaled,
     ),
@@ -298,20 +307,20 @@ EVAL_CASES: list[EvalCase] = [
     EvalCase(name="error_incompatible_units", expression="arcsec + mag", columns={}, error=True),
     EvalCase(
         name="error_string_plus_number",
-        expression='col("name") + col("logd25")',
-        columns={"name": Col("x"), "logd25": Col(1.5)},
+        expression='col("err_name") + col("logd25")',
+        columns=_COLUMNS,
         error=True,
     ),
     EvalCase(
         name="error_modulo_dimensionless_divisor",
         expression='col("pa") % 180.0',
-        columns={"pa": Col(190.0, "deg")},
+        columns=_COLUMNS,
         error=True,
     ),
     EvalCase(
         name="error_trig_on_dimensionless",
-        expression='sin(col("x"))',
-        columns={"x": Col(0.5)},
+        expression='sin(col("x_dimless"))',
+        columns=_COLUMNS,
         error=True,
     ),
 ]
